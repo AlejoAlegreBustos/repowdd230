9aaac6e59245dc2a94292d2ec8eff7a3a103a7d7
AlejoAlegreBustos <ale21020@byui.edu>
2022-10-08 15:06:09 -0300

    upates chamber site

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,7 +1,7 @@
 const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.nav_bar')
+const mainnav = document.getElementById('#navegation')
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
 // To solve the mid resizing issue with responsive class on
-window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+window.onresize = () => {if (window.innerWidth > 560) mainnav.classList.remove('responsive')};
