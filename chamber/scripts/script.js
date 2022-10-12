0a698b5d9439ae320a3f870aefe6ec1cbbcac39d
AlejoAlegreBustos <ale21020@byui.edu>
2022-10-11 23:14:53 -0300

    last last update chamber home page

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,7 +1,5 @@
-const hambutton = document.querySelector('.ham');
-const mainnav = document.getElementById('#navegation')
-
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
-
-// To solve the mid resizing issue with responsive class on
-window.onresize = () => {if (window.innerWidth > 560) mainnav.classList.remove('responsive')};
+function toggleMenu(){
+    document.getElementById("primaryNav").classList.toggle("open");
+}
+const bottonn=document.getElementById("hamBtn");
+bottonn.onclick=toggleMenu;
--- chamber/scripts/scripts_last_updated.js
+++ chamber/scripts/scripts_last_updated.js
@@ -1,5 +1,2 @@
-function toggleMenu(){
-    document.getElementById("primaryNav").classList.toggle("open");
-}
-const bottonn=document.getElementById("hamBtn");
-bottonn.onclick=toggleMenu;
+let oLastModif = new Date(document.lastModified)
+document.querySelector('#date').textContent=oLastModif
